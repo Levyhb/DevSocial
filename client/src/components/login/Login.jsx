6df113feca98b0b895ff9272b0a194bd7dfcae7e
levyhb <levybholanda@gmail.com>
2022-12-23 18:33:50 -0300

    fix login page - email and password verification

--- client/src/components/login/Login.jsx
+++ client/src/components/login/Login.jsx
@@ -1,30 +1,46 @@
-import React from 'react';
+import React, { useRef } from "react";
 import "./login.css";
 
 export default function Login() {
+  const email = useRef();
+  const password = useRef();
+  const handleClick = (e) => {
+    e.preventDefault();
+  };
   return (
     <div className="login">
       <div className="loginWrapper">
         <div className="loginLeft">
           <h3 className="loginLogo">ReactSocial</h3>
           <span className="loginDesc">
-            Connect with firends and the world around you on ReactSocial.
+            Connect with friends and the world around you on ReactSocial.
           </span>
         </div>
         <div className="loginRight">
-          <div className="loginBox">
-            <input placeholder="Email" className="loginInput" />
-            <input placeholder="Password" className="loginInput" />
-            <button className="loginButton">
-              Log In
-            </button>
-            <span className='loginForgot'>Forgot Password?</span>
+          <form className="loginBox" onSubmit={handleClick}>
+            <input
+              placeholder="Email"
+              className="loginInput"
+              type="email"
+              required
+              ref={email}
+            />
+            <input
+              placeholder="Password"
+              className="loginInput"
+              type="password"
+              minLength="6"
+              required
+              ref={password}
+            />
+            <button className="loginButton">Log In</button>
+            <span className="loginForgot">Forgot Password?</span>
             <button className="loginRegisterButton">
               Create a New Account
             </button>
-          </div>
+          </form>
         </div>
       </div>
     </div>
-  )
+  );
 }
