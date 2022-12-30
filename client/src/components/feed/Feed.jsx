4e5a9c44ce98a7f994dab3f982087ecdf1655fef
levyhb <levybholanda@gmail.com>
2022-12-30 11:07:47 -0300

    fix: timeline

--- client/src/components/feed/Feed.jsx
+++ client/src/components/feed/Feed.jsx
@@ -16,7 +16,9 @@ export default function Feed({ username }) {
       const res = username
         ? await axios.get(`/posts/profile/${username}`)
         : await axios.get("/posts/timeline/"+ user._id);
-      setPost(res.data);
+      setPost(res.data.sort((p1, p2) => {
+        return new Date(p2.createdAt) - new Date(p1.createdAt);
+      }));
     };
     fetchPost();
   }, [username, user._id]);
