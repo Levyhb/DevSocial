854ead0ad09e15210744631aa65a5a3ec5a022bd
levyhb <levybholanda@gmail.com>
2023-01-07 19:22:18 -0300

    feat: logout system
    temporaly logout system

--- client/src/components/topbar/Topbar.jsx
+++ client/src/components/topbar/Topbar.jsx
@@ -1,12 +1,20 @@
 import React, { useContext } from "react";
 import "./topbar.css";
 import { Search, Person, Chat, Notifications } from "@mui/icons-material";
-import { Link } from "react-router-dom";
+import { Link, redirect } from "react-router-dom";
 import { AuthContext } from "../../context/AuthContext";
+import QuestionAnswerIcon from '@mui/icons-material/QuestionAnswer';
+import FeedIcon from '@mui/icons-material/Feed';
+import LogoutIcon from '@mui/icons-material/Logout';
 
 export default function Topbar() {
   const { user } = useContext(AuthContext);
   const PF = process.env.REACT_APP_PUBLIC_FOLDER;
+
+  const handleLogout = () => {
+    localStorage.removeItem("user");
+    window.location.reload();
+  }
 
   return (
     <div className="topbarContainer">
@@ -25,14 +33,14 @@ export default function Topbar() {
       <div className="topbarRight">
         <div className="topbarLinks">
           <Link to="/" className="topbarLink">
-            <span>Timeline</span>
+            <span>Timeline <FeedIcon /></span>
           </Link>
           <Link to={"/messenger"} className="topbarLink">
-            <span>Messenger</span>
+            <span>Messenger < QuestionAnswerIcon /></span>
           </Link>
         </div>
 
-        <div>
+        {/* <div>
           <div className="topbarIcons">
             <div className="topbarIconItem">
               <Person />
@@ -49,7 +57,7 @@ export default function Topbar() {
               <div className="topbarIconItemBadge">1</div>
             </div>
           </div>
-        </div>
+        </div> */}
         <Link to={`/profile/${user.username}`}>
           <img
             src={
@@ -61,6 +69,10 @@ export default function Topbar() {
             className="topbarImg"
           />
         </Link>
+
+        <Link to={"/login"} className="logout" onClick={handleLogout}>
+          <LogoutIcon/>
+        </Link >
       </div>
     </div>
   );
