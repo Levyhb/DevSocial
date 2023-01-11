7313af7d77b78cb52f2ddac97239268db523e7e6
levyhb <levybholanda@gmail.com>
2023-01-10 23:28:49 -0300

    fix: error with routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,28 @@
-import { Route, Routes, redirect } from "react-router-dom";
+import { Route, Routes, redirect, useLocation, Navigate } from "react-router-dom";
 import Login from "./pages/login/Login";
 import Register from "./pages/register/register";
 import Home from "./pages/home/Home";
 import Profile from "./pages/profile/Profile";
-import { useContext } from "react";
+import { useContext, useEffect } from "react";
 import { AuthContext } from "./context/AuthContext";
 import Messenger from "./pages/messenger/Messenger";
 
 function App() {
   const { user } = useContext(AuthContext);
+  const location = useLocation();
+
+  useEffect(() => {
+    if(!user) redirect("/login")
+  }, [location])
 
   return (
     <div>
       <Routes>
         <Route exact path="/" element={user ? <Home /> : <Login />} />
-        <Route path="/login" element={user ? redirect("/") : <Login />} />
-        <Route path="/register" element={user ? redirect("/") : <Register />} />
-        <Route path="/profile/:username" element={<Profile />} />
-        <Route path="/messenger" element={!user ? redirect("/login") : <Messenger />} />
+        <Route path="/login" element={user ? <Navigate replace to="/" /> : <Login />} />
+        <Route path="/register" element={user ? <Navigate replace to="/" /> : <Register />} />
+        <Route path="/profile/:username" element={user ? <Profile /> : <Navigate replace to="/   " />} />
+        <Route path="/messenger" element={!user ? <Navigate replace to="/" /> : <Messenger />} />
       </Routes>
 
     </div>
