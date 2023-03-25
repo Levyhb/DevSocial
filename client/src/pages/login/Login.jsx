0b6c60c4d574a5f8eba8e76333a4552ef5c57354
Levy Bezerra <94788307+Levyhb@users.noreply.github.com>
2023-03-24 23:49:20 -0300

    fix: login title

--- client/src/pages/login/Login.jsx
+++ client/src/pages/login/Login.jsx
@@ -28,7 +28,7 @@ export default function Login() {
         <div className="loginLeft">
           <h3 className="loginLogo">&lt; DevSocial /&gt;	</h3>
           <span className="loginDesc">
-            Connect with friends and the world around you on ReactSocial.
+            Connect with friends and the world around you on DevSocial.
           </span>
         </div>
         <div className="loginRight">
